75e3a813b87906365bf27939fb4a1fe485051a50
Nonouille <arnaud.py@edu.devinci.fr>
2024-02-05 14:47:52 +0100

    Decision Tree making

---
+++ app.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load dataset
+titanic_data = pd.read_csv('titanic.csv')
+# Explore the dataset
+print(titanic_data.head())
+
+
+# Handle missing values
+titanic_data = titanic_data.dropna(subset=['Embarked'])
+titanic_data['Age'].fillna(titanic_data['Age'].median(), inplace=True)
+# Encode categorical variables
+titanic_data = pd.get_dummies(titanic_data, columns=['Sex', 'Embarked'], drop_first=True)
+
+
+# Select features and target variable
+X = titanic_data[['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex_male', 'Embarked_Q', 'Embarked_S']]
+y = titanic_data['Survived']
+#Train Test Split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Create Decision Tree model
+dt_model = DecisionTreeClassifier(random_state=42,max_depth=3)
+
+# Train the model
+dt_model.fit(X_train, y_train)
+
+# Predictions on the test set
+y_pred = dt_model.predict(X_test)
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+# Confusion Matrix and Classification Report
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+print('Confusion Matrix:')
+print(conf_matrix)
+print('\nClassification Report:')
+print(class_report)
+
+
+#Visualize the decision tree
+from sklearn.tree import plot_tree
+plt.figure(figsize=(12, 8))
+plot_tree(dt_model, feature_names=X.columns, class_names=['Not Survived', 'Survived'], filled=True, rounded=True)
+plt.show()
+
