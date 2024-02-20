cdfc6d5c65170e9f534f982fab9fbafafa9b1cf9
Nonouille <arnaud.py@edu.devinci.fr>
2024-02-20 01:02:45 +0100

    Add JS

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const client = new Client({
 
 client.connect();
 
-
 let products = []
 let orders = []
 let orderDetails = []
@@ -40,11 +39,14 @@ async function updateDatabase() {
 
     const resultCartDetails = await client.query("SELECT * FROM cartDetails");
     cartDetails = resultCartDetails.rows;
+
   }
   catch (err) {
     console.error("Error initializing the database", err);
   }
   finally {
+    let actualID = products.length;
+    let orderID = orders.length;
     console.log("Database retrieved");
   }
 }
@@ -116,7 +118,7 @@ app.post('/products', async (req, res) => {
     `;
 
   // Use the pg library to perform a bulk insert
-  const result = await client.query(query, values.flat());
+  const result = await client.query(query, values);
 
   // Check if a record was updated
   if (result.rowCount === 0) {
@@ -205,43 +207,63 @@ app.delete('/products/:id', async(req, res) => {
 //###################################
 
 //Create an order
-app.post('/orders' , (req,res) => {
+app.post('/orders' , async (req,res) => {
   //create ID and usefull variables
   thisOrderID = generateUniqueIdOrder();
   totalPrice = 0;
   odrerDetail = [];
-  bodyDetails = [];
+
   //retrieve body details
   const body = req.body;
   oderUser = body.user.toLowerCase();
   bodyDetails = body.order;
+
   //for each element of the order, retrieve ID and calculate total price
-  bodyDetails.forEach( element => {
+  for (const element of bodyDetails) {
     const productIndex = products.findIndex(product => product.id === element.id);
-    element = {
-      "id" : element.id,
-      "Quantity" : element.qty,
-      "Name" : products[productIndex].name,
-      "Price" : products[productIndex].price};
-    odrerDetail.push(element);
-    totalPrice += products[productIndex].price*element.qty
-  });
+    if (productIndex === -1) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    //create the new order json element
+    const query = `
+    INSERT INTO orderDetails(orderid, id, quantity, "name", price)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *
+    `;
+    const values = [thisOrderID, element.id, element.qty, products[productIndex].name, products[productIndex].price];
+    // Use the pg library to execute the insert query
+    await client.query(query, values);
+    totalPrice +=  products[productIndex].price*element.qty
+  };
   //round total price
   totalPrice = Math.round(totalPrice*100)/100;
+  console.log(totalPrice);
   //create the new order json element
-  newOrder = {"orderID" : thisOrderID, "status" : "pending", "User" : oderUser, "odrerDetail" : odrerDetail, "Total Price" : totalPrice}
-  //add the new order to the array orders
-  orders.push(newOrder);
-  res.status(200).json({"message" : "Sucessful order creation", "orderDetails" : newOrder});
+  const query = `
+      INSERT INTO orders(orderid, "status", "user", totalprice)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+    `;
+  const values = [thisOrderID, "pending", oderUser, totalPrice];
+  // Use the pg library to execute the insert query
+  const result = await client.query(query, values);
+
+  updateDatabase();
+  // Respond with the created order details
+  res.status(200).json({
+    message: 'Successful order creation',
+    orderDetails: result.rows[0],
+  });
 })
 
 //Get orders by a userId
 app.get('/orders/:userId', (req,res) => {
+  updateDatabase();
   const userId = req.params.userId;
   if (!userId) {
     return res.status(404).json({ error: 'User not found' });
   }
-  filteredOrders = orders.filter(order => order.User === userId.toLowerCase());
+  filteredOrders = orders.filter(order => order.user === userId.toLowerCase());
   res.status(200).json(filteredOrders);
 })
 
@@ -250,25 +272,29 @@ app.get('/orders/:userId', (req,res) => {
 //###################################
 
 //Add a product to user cart
-app.post('/cart/:userId',(req,res) => {
+app.post('/cart/:userId', async (req,res) => {
+  let totalQuantity = 0;
+  let totalPrice = 0;
   const userId = req.params.userId.toLowerCase();
   if (!userId) {
     return res.status(404).json({ error: 'User not found' });
   }
-
   //Get the cart if existing 
+  thisUserCartDetail = cartDetails.filter(order => order.user === userId);
   thisUserCart = carts.filter(order => order.user === userId);
   //Create a new one if not existing
   if (thisUserCart.length === 0){
-    carts.push(thisUserCart = {
-      "user" : userId,
-      "cartDetail" : [],
-      "totalPrice" : 0
-    })
+    const insertQuery = `
+        INSERT INTO carts("user", cartstatus, totalprice)
+        VALUES($1, $2, $3)
+        RETURNING *
+      `;
+
+    const values = [userId, 1, 0];
+    const insertResult = await client.query(insertQuery, values);
+
+    thisUserCart = insertResult.rows[0];
   }
-  
-  //Get the index in the carts array for this cart
-  cartIndex = carts.findIndex(cart => cart.user === userId);
 
   //Get infos about the product
   const body = req.body;
@@ -276,38 +302,62 @@ app.post('/cart/:userId',(req,res) => {
   if (productIndex === -1) {
     return res.status(404).json({ error: 'Product not found' });
   }
-
   //If product already in cart, just increase the qty
-  thisCartObject = carts[cartIndex].cartDetail.filter(object => object.id === body.id)
+  thisCartObject = thisUserCartDetail.filter(object => object.id === body.id)
   if(thisCartObject.length != 0){
-    const cartDetailIndex = carts[cartIndex].cartDetail.findIndex(object => object.id === body.id)
-    carts[cartIndex].cartDetail[cartDetailIndex].Quantity += body.qty;
+    const updateQuery = `
+      UPDATE cartDetails
+      SET quantity = $2
+      WHERE "user" = $3 AND id = $1
+      RETURNING *
+    `;
+    totalQuantity = thisCartObject.reduce((acc, item) => acc + item.quantity, 0) + body.qty;
+    const updateValues = [body.id,totalQuantity, userId];
+    const updateResult = await client.query(updateQuery, updateValues);
   }
   else {
-    addToCartDetail = {
-      "id" : body.id,
-      "Quantity" : body.qty,
-      "Name" : products[productIndex].name,
-      "Price" : products[productIndex].price
-    };
-    carts[cartIndex].cartDetail.push(addToCartDetail);
-  } 
-  carts[cartIndex].totalPrice+= (products[productIndex].price*body.qty);
-  res.status(200).json({"message" : "Added to cart", "cart" : carts[cartIndex]});
+    const insertQuery = `
+      INSERT INTO cartDetails( "user", id, quantity, name, price)
+      VALUES($1, $2, $3, $4, $5)
+      RETURNING *
+    `;
+    const insertValues = [userId, body.id, body.qty, products[productIndex].name, products[productIndex].price];
+    const updateResult = await client.query(insertQuery, insertValues);
+  }
+
+  const updateQuery = `
+  UPDATE carts
+  SET "user" = $1, cartstatus = $2, totalprice = $3
+  WHERE "user" = $1
+  RETURNING *
+  `;
+  
+  totalPrice = thisUserCart.reduce((acc,item) => acc + item.totalPrice,0) + (products[productIndex].price*body.qty);
+  const updateValues = [userId, 1, totalPrice];
+  const updateResult = await client.query(updateQuery, updateValues);
+
+  updateDatabase();
+  // Respond with the updated cart details
+  res.status(200).json({
+    message: 'Added to cart',
+    cart: updateResult.rows[0],
+  });
 })
 
 //Get cart from a userID
 app.get('/cart/:userId', (req,res) => {
+  updateDatabase();
   const userId = req.params.userId;
   if (!userId) {
     return res.status(404).json({ error: 'User not found' });
   }
-  filteredCarts = carts.filter(cart => cart.user === userId.toLowerCase());
+  filteredCarts = cartDetails.filter(cart => cart.user === userId.toLowerCase());
+  filteredCarts += carts.filter(cart => cart.user === userId.toLowerCase());
   res.status(200).json(filteredCarts[0]);
 })
 
 //Delete an item
-app.delete('/cart/:userId/item/:productId', (req,res) => {
+app.delete('/cart/:userId/item/:productId', async (req,res) => {
   const userId = req.params.userId;
   if (!userId) {
     return res.status(404).json({ error: 'User not found' });
@@ -317,21 +367,34 @@ app.delete('/cart/:userId/item/:productId', (req,res) => {
     return res.status(400).json({ error: 'productId incorrect' });
   }
   //Verify the user cart exist
-  thisUserCart = carts.filter(order => order.user === userId);
+  thisUserCart = cartDetails.filter(order => order.user === userId);
   if(thisUserCart.length === 0){
     return res.status(400).json({ error: 'This user has no actual cart' });
   }
-  //Get the index in the carts array for this cart
-  cartIndex = carts.findIndex(cart => cart.user === userId);
   
   //Verify the object is in the user cart 
-  thisCartObject = carts[cartIndex].cartDetail.filter(object => object.id === productId);
+  thisCartObject = thisUserCart.filter(object => object.id === productId);
   if(thisCartObject.length === 0){
-    return res.status(400).json({ error: 'This user doesn\' have this object in the actual cart' });
+    return res.status(400).json({ error: 'This user doesn\'t have this object in the actual cart' });
   }
-  const cartDetailIndex = carts[cartIndex].cartDetail.findIndex(object => object.id === productId);
-  deletedItem = carts[cartIndex].cartDetail.splice(cartDetailIndex, 1)[0];
-  res.status(200).json({"message" : "Sucesfully deleted ","deletedItem" : deletedItem});
+  // Update the cart in the database
+  const deleteQuery = `
+    DELETE FROM cartDetails
+    WHERE id = $1 AND "user" = $2
+    RETURNING *
+  `;
+  const deleteValues = [thisCartObject.id,thisCartObject.user];
+  const deleteResult = await client.query(deleteQuery, deleteValues);
+
+  // Check if a cart was deleted
+  if (deleteResult.rowCount === 0) {
+    return res.status(404).json({ error: 'Cart not found' });
+  }
+  // Respond with the deleted cart details
+  res.status(200).json({
+    message: 'Successfully deleted',
+    deletedCart: deleteResult.rows[0],
+  });
 })
 
 console.log('starting...');
